05091e5a26ab719ec0812490603564f418664a69
seocodes <azanettev@gmail.com>
2025-03-13 16:33:32 -0300

    fiz o mini exercicio

--- components/Test.js
+++ components/Test.js
@@ -1,17 +1,42 @@
 import { Text, View, StyleSheet, Image } from "react-native";
 
 export default function Test(){
+    return(
     <View>
-        <View>
-            <Text>KFDJSKFDSJKFSDJKFDSJFKDSJ</Text>
-            //IMAGEM NAO TA INDO
-            <Image style={{ width: 200, height: 200}} source={require('../assets/images.jpg')} />
-            <Text>dkfasjgffs</Text>
-            <Image style={{width: 200, height: 200}} source={require('../assets/jojo.jpg')} />
+        <View style={styles.container}>
+            <View style={styles.containerTextImage}>
+                <Text>KFDJSKFDSJKFSDJKFDSJFKDSJ</Text>
+                {/* IMAGEM NAO TA INDO */}
+                <Image style={{ width: 200, height: 200}} source={require('../assets/images.jpg')} />
+            </View>
+            <Text>Ai num sei :3</Text>
+            <View style={styles.containerTextImage2}>
+                <Text>dkfasjgffs</Text>
+                <Image style={{width: 200, height: 200}} source={require('../assets/jojo.jpg')} />
+            </View>
+            <Text style={styles.textoProLado}>alguma coisa</Text>
         </View>
     </View>
+    )
 }
 
 const styles = StyleSheet.create({
-
+    container: {
+        flex: 1, 
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      },
+      containerTextImage: {
+        flexDirection: "row",
+        margin: 50,
+      },
+      containerTextImage2: {
+        flexDirection: "row-reverse",
+        margin: 50,
+      },
+      textoProLado: {
+        alignSelf: "flex-end",
+        marginHorizontal: 20,
+      }
 })
